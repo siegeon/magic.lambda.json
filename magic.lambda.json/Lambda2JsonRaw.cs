9770f14e0e0cffd6769151f1170d1de66d2f319a
Thomas Hansen <thomas@gaiasoul.com>
2020-09-11 10:43:34 +0300

    Removing code smells

--- magic.lambda.json/Lambda2JsonRaw.cs
+++ magic.lambda.json/Lambda2JsonRaw.cs
@@ -3,9 +3,7 @@
  * See the enclosed LICENSE file for details.
  */
 
-using System.Linq;
 using magic.node;
-using magic.node.extensions;
 using magic.signals.contracts;
 using magic.lambda.json.utilities;
 
