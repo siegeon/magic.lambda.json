ed33bc727bce0d4bd178c02820a683c3248eb921
Thomas Hansen <thomas@gaiasoul.com>
2021-12-21 12:57:58 +0200

    Implementing IMagicConfiguration

--- magic.lambda.json.tests/Common.cs
+++ magic.lambda.json.tests/Common.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using magic.node;
+using magic.node.contracts;
 using magic.signals.services;
 using magic.signals.contracts;
 using magic.node.extensions.hyperlambda;
@@ -34,9 +36,10 @@ namespace magic.lambda.json.tests
 
         static IServiceProvider Initialize()
         {
-            var configuration = new ConfigurationBuilder().Build();
             var services = new ServiceCollection();
-            services.AddTransient<IConfiguration>((svc) => configuration);
+            var mockConfiguration = new Mock<IMagicConfiguration>();
+            mockConfiguration.SetupGet(x => x[It.IsAny<string>()]).Returns("bar-xx");
+            services.AddTransient((svc) => mockConfiguration.Object);
             services.AddTransient<ISignaler, Signaler>();
             var types = new SignalsProvider(InstantiateAllTypes<ISlot>(services));
             services.AddTransient<ISignalsProvider>((svc) => types);
